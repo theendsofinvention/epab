bb661a6b8311a3988ce2aae3113346c672b475b6
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-16 16:26:24 +0100

    new: add flake8 params as default

--- epab/linters/__init__.py
+++ epab/linters/__init__.py
@@ -32,7 +32,14 @@ def flake8(ctx):
     """
     Runs Flake8 (http://flake8.pycqa.org/en/latest/)
     """
-    do(ctx, ['flake8'])
+    ignore = ['--ignore=D203,E126']
+    max_line_length = ['--max-line-length=120']
+    # noinspection SpellCheckingInspection
+    exclude = ['--exclude', '_version.py,_*_version.py,_versioneer.py,versioneer.py,.svn,CVS,.bzr,.hg,.git,'
+                            '__pycache__,.tox,__init__.py,dummy_miz.py,build,dist,output,.cache,.hypothesis,'
+                            'qt_resource.py,_parking_spots.py,./test/*,./.eggs/*,']
+    max_complexity = ['--max-complexity=10']
+    do(ctx, ['flake8'] + ignore + max_line_length + exclude + max_complexity)
 
 
 @click.command()
