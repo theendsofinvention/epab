275f4e8c61ed875e460e3e81899d838fbef51e54
132nd-etcher <132nd-etcher@daribouca.net>
2017-12-16 18:01:20 +0100

    fix: sanitize console output

--- epab/utils/_console.py
+++ epab/utils/_console.py
@@ -4,19 +4,29 @@ Manages output functions
 """
 
 import click
+import io
+
+
+def _sanitize(input_: str):
+    in_stream = input_.encode('utf8')
+    return in_stream.decode(errors='replace')
 
 
 def _info(txt: str, **args):
+    txt = _sanitize(txt)
     click.secho(txt, fg='green', **args)
 
 
 def _error(txt: str, **args):
+    txt = _sanitize(txt)
     click.secho(txt, fg='red', err=True, **args)
 
 
 def _cmd(txt: str, **args):
+    txt = _sanitize(txt)
     click.secho(txt, fg='magenta', **args)
 
 
 def _out(txt: str, **args):
+    txt = _sanitize(txt)
     click.secho(txt, fg='cyan', **args)
