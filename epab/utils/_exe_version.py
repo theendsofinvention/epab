136a670f97a068aa9e84455ad5be14d700046259
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-24 19:05:12 +0100

    add methods to retrieve version from exe

---
+++ epab/utils/_exe_version.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+"""
+Get version info from executable
+"""
+import typing
+from pathlib import Path
+
+import pefile
+
+
+def _low_word(dword):
+    return dword & 0x0000ffff
+
+
+def _high_word(dword):
+    return dword >> 16
+
+
+class VersionInfo:
+    """
+    Simple version info
+    """
+
+    def __init__(self, file_version, full_version):
+        self._file_version = file_version
+        self._full_version = full_version
+
+    @property
+    def file_version(self) -> str:
+        """
+        Returns: simple version
+        """
+        return self._file_version
+
+    @property
+    def full_version(self) -> str:
+        """
+        Returns: full version
+        """
+        return self._full_version
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.file_version}, {self.full_version})'
+
+    def __str__(self):
+        return self.file_version
+
+
+def get_product_version(path: typing.Union[str, Path]) -> VersionInfo:
+    """
+    Get version info from executable
+
+    Args:
+        path: path to the executable
+
+    Returns: VersionInfo
+    """
+    path = Path(path).absolute()
+    pe = pefile.PE(str(path))
+
+    for file_info in pe.FileInfo:
+        if file_info.Key == b'StringFileInfo':
+            for st in file_info.StringTable:
+                if b'FileVersion' in st.entries.keys():
+                    file_version = st.entries[b'FileVersion'].decode('utf8')
+                    full_version = st.entries[b'PrivateBuild'].decode('utf8')
+                    return VersionInfo(file_version, full_version)
+    else:
+        raise RuntimeError(f'unable to obtain version from {path}')
+
+
+if __name__ == '__main__':
+    print(get_product_version(r'F:\DEV\EPAB\dist\epab.exe'))
