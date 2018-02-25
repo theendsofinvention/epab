631088d02b1c5735892b60ced3f7cf7ac1514e3a
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-25 07:47:12 +0100

    fix frozen version

--- epab/core/version.py
+++ epab/core/version.py
@@ -3,11 +3,17 @@
 EPAB current version
 """
 
+import sys
 
 from pkg_resources import DistributionNotFound, get_distribution
+
+from epab.utils._exe_version import get_product_version
 
 try:
     VERSION = get_distribution('epab').version
 except DistributionNotFound:  # pragma: no cover
-    # package is not installed
-    VERSION = 'not installed'
+    if getattr(sys, 'frozen', False):
+        VERSION = get_product_version(sys.executable)
+    else:
+        # package is not installed
+        VERSION = 'not installed'
