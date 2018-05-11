6bf04955cc912bb5ad652897a80e8b2976dfc56a
132nd-etcher <132nd-etcher@daribouca.net>
2018-05-11 14:57:55 +0200

    create sample config if it doesn't exist

--- epab/core/config.py
+++ epab/core/config.py
@@ -4,6 +4,8 @@ Handles EPAB's config file
 """
 
 import sys
+import inspect
+from collections import defaultdict
 from pathlib import Path
 
 import yaml
@@ -15,12 +17,13 @@ TEST_VALUES = {
 
 class _ConfigProp:
 
-    def __init__(self, default=None, mandatory=False, cast=None):
+    def __init__(self, default=None, mandatory=False, cast=None, help_str=''):
         if default and mandatory:
             raise RuntimeError('Config option can\'t have a default and be mandatory at the same time')
         self.default = default
         self.mandatory = mandatory
         self.cast = cast
+        self.help = help_str
 
     # pylint: disable=inconsistent-return-statements
     def __set__(self, instance, value):
@@ -63,7 +66,51 @@ class _Config:
     def __init__(self):
         self._data = {}
 
-    def load(self, config_file='epab.yml'):
+    @classmethod
+    def make_default(cls):
+
+        def _add_attributes(attr_list, padding=''):
+            nonlocal default_config
+            for _attr in sorted(attr_list, key=lambda x: x.name):
+                assert isinstance(_attr, _ConfigProp)
+                try:
+                    _, _attr_name = _attr.name.split('__')
+                except ValueError:
+                    _attr_name = _attr.name
+
+                default_config += f'{padding}# {_attr.help}\n{padding}# Type: {_attr.cast}\n'
+                default_config += padding
+                if not _attr.mandatory and not _attr.default:
+                    default_config += '# '
+                default_config += f'{_attr_name}:'
+                if _attr.default:
+                    default_config += f' {_attr.default}'
+                default_config += '\n\n'
+
+        attributes = defaultdict(list)
+        for attr_name, attr in inspect.getmembers(cls):
+            # print(attr_name, attr)
+            if isinstance(attr, _ConfigProp):
+                try:
+                    section, attr_name = attr_name.split('__')
+                    attributes[section].append(attr)
+                except ValueError:
+                    attributes['root'].append(attr)
+        default_config = '# EPAB configuration file\n\n'
+        _add_attributes(attributes['root'])
+        del attributes['root']
+        for section in sorted(attributes.keys()):
+            for attr in attributes[section]:
+                if attr.default or attr.mandatory:
+                    break
+            else:
+                default_config += '#'
+            default_config += f'{section}:\n'
+            _add_attributes(attributes[section], padding='\t')
+        Path('epab.yml').write_text(default_config)
+
+
+    def load(self, config_file='epab.ymml'):
         """
         Loads configuration data from a config file
 
@@ -73,6 +120,7 @@ class _Config:
         config_file = Path(config_file).absolute()
         if not config_file.exists():
             if not hasattr(sys, '_called_from_test'):
+                self.make_default()
                 raise FileNotFoundError(f'Config file not found: {config_file}')
             self._data = TEST_VALUES
         else:
@@ -102,23 +150,23 @@ class Config(_Config):
     """
     Manages configuration for EPAB
     """
-    changelog__disable = _ConfigProp(cast=bool)
-    changelog__file = _ConfigProp(default='CHANGELOG.rst')
-    test__av_runner_options = _ConfigProp(cast=str)
-    test__runner_options = _ConfigProp(cast=str)
-    test__duration = _ConfigProp(default='10', cast=str)
-    test__target = _ConfigProp(default='test', cast=str)
-    test__coverage__fail_under = _ConfigProp(cast=int, default=20)
-    lint__line_length = _ConfigProp(default='120', cast=str)
-    package = _ConfigProp(mandatory=True)
-    freeze__entry_point = _ConfigProp(cast=str, default='')
-    freeze__data_files = _ConfigProp(cast=tuple, default=tuple())
-    doc__repo = _ConfigProp(cast=str)
-    doc__folder = _ConfigProp(default='./doc')
-    quiet = _ConfigProp(default=False, cast=bool)
-    verbose = _ConfigProp(default=False, cast=bool)
-    artifacts = _ConfigProp(cast=list)
-    flake8__exclude = _ConfigProp(cast=str, default='')
+    changelog__disable = _ConfigProp(cast=bool, help_str='Disable changelog creation')
+    changelog__file = _ConfigProp(default='CHANGELOG.rst', help_str='Changelog file name')
+    test__av_runner_options = _ConfigProp(default='--long', cast=str, help_str='PyTest runner options on Appveyor')
+    test__runner_options = _ConfigProp(default='', cast=str, help_str='PyTest runner options')
+    test__duration = _ConfigProp(default='10', cast=str, help_str='Number of slow tests to show')
+    test__target = _ConfigProp(default='test', cast=str, help_str='PyTest target')
+    coverage__fail_under = _ConfigProp(cast=int, default=20, help_str='Minimal coverage to pass tests')
+    lint__line_length = _ConfigProp(default='120', cast=str, help_str='Maximum code line length')
+    package = _ConfigProp(mandatory=True, cast=str, help_str='Package name')
+    freeze__entry_point = _ConfigProp(cast=str, default='', help_str='Entry point for scripts')
+    freeze__data_files = _ConfigProp(cast=tuple, default=tuple(), help_str='Packaging data files')
+    doc__repo = _ConfigProp(cast=str, help_str='Documentation repository')
+    doc__folder = _ConfigProp(default='./doc', help_str='Documentation folder')
+    quiet = _ConfigProp(default=False, cast=bool, help_str='Less output')
+    verbose = _ConfigProp(default=False, cast=bool, help_str='More output')
+    artifacts = _ConfigProp(cast=list, help_str='List of artifacts for AppVeyor')
+    flake8__exclude = _ConfigProp(cast=str, default='', help_str='List of files excluded from flake8 analysis')
 
 
 CONFIG = Config()
