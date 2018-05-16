dda10fec6a5efae88a2bcbbd2870abf9ce2bc5c6
132nd-etcher <132nd-etcher@daribouca.net>
2018-05-16 09:00:35 +0200

    fix linters test

--- test/test_cmd/test_linters.py
+++ test/test_cmd/test_linters.py
@@ -8,7 +8,7 @@ from mockito import mock, verify, verifyNoMoreInteractions, verifyStubbedInvocat
 
 import epab.utils
 from epab.core import CONFIG, CTX
-from epab.linters import _flake8, _lint, _pep8, _pylint, _safety, _sort
+from epab.linters import _flake8, _lint, _pep8, _pylint, _safety, _sort, _mypy
 
 
 @pytest.fixture(autouse=True, name='repo')
@@ -29,6 +29,7 @@ def test_lint(amend_stage):
     verify(context).invoke(_safety.safety)
     verify(context).invoke(_pylint.pylint)
     verify(context).invoke(_flake8.flake8)
+    verify(context).invoke(_mypy.mypy)
     verify(context).invoke(_sort.sort, amend=amend, stage=stage)
     verify(context).invoke(_pep8.pep8, amend=amend, stage=stage)
     verifyNoMoreInteractions(context)
@@ -46,6 +47,7 @@ def test_lint_appveyor(amend_stage):
     verify(context).invoke(_safety.safety)
     verify(context).invoke(_pylint.pylint)
     verify(context).invoke(_flake8.flake8)
+    verify(context).invoke(_mypy.mypy)
     verify(context).invoke(_pep8.pep8, amend=amend, stage=stage)
     verifyNoMoreInteractions(context)
 
