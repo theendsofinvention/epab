248ab788fcc509898c90ef7708f092e4fc0a4b16
etcher <github@daribouca.net>
2018-08-26 15:23:15 +0200

    linting

--- epab/utils/__init__.py
+++ epab/utils/__init__.py
@@ -11,6 +11,6 @@ from ._gitignore import add_to_gitignore
 from ._next_version import get_next_version
 from ._repo import Repo
 from ._resource_path import resource_path
-from ._run import run, run
+from ._run import run
 from ._run_once import run_once
 from ._stashed import stashed
