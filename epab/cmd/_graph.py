a836165f3925eb3dff5898df1becdfbc1ac8dbdd
etcher <github@daribouca.net>
2018-10-21 09:29:47 +0200

    fix: remove existing graph files

--- epab/cmd/_graph.py
+++ epab/cmd/_graph.py
@@ -2,6 +2,7 @@
 """
 Wrapper around pyreverse (needs Graphviz in the path)
 """
+import logging
 from pathlib import Path
 
 import click
@@ -9,13 +10,18 @@ import elib_run
 
 from epab.core import config
 
+LOGGER = logging.getLogger('EPAB')
+
 
 def _graph():
     out_files = [
-        Path(f'packages_{config.PACKAGE_NAME()}.png'),
-        Path(f'classes_{config.PACKAGE_NAME()}.png'),
-
+        Path(f'packages_{config.PACKAGE_NAME()}.png').absolute(),
+        Path(f'classes_{config.PACKAGE_NAME()}.png').absolute(),
     ]
+    for _file in out_files:
+        if _file.exists:
+            logging.debug('removing graph file: %s', _file)
+
     elib_run.run(f'pyreverse -o png -p {config.PACKAGE_NAME()} {config.PACKAGE_NAME()}')
     if any((file.exists() for file in out_files)):
         out_dir = Path('graphs')
